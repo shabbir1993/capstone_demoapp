f7eac043d40de53bba92ff7c1cd1a51a877f5838
Shabbir Haider <syed_shabbir_haider@hotmail.com>
2017-08-29 13:31:34 +0000

    added rspec and some tests

---
+++ spec/requests/api_developments_spec.rb
@@ -0,0 +1,19 @@
+#require 'rails_helper'
+
+RSpec.describe "ApiDevelopments", type: :request do
+  describe "RDBMS-backed resource" do
+    it "creates an RDBMS backed resource" do
+    end
+    
+    it 'exposes RDBMS backed resource api' do
+    end
+  end
+  
+  describe 'MongoDB-backed resource' do
+    it 'creates MongoDB backed resource'do
+    end
+    
+    it 'exposes MongoDB backed api resource' do
+    end
+  end
+end
